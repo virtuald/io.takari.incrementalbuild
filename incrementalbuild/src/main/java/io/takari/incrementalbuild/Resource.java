1eb04a430564aa5719f36d80dc57f4c2072c1bb3
Igor Fedorenko <igor@ifedorenko.com>
2015-03-07 09:05:39 -0500

    YYY cleanup
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- incrementalbuild/src/main/java/io/takari/incrementalbuild/Resource.java
+++ incrementalbuild/src/main/java/io/takari/incrementalbuild/Resource.java
@@ -5,12 +5,10 @@ import java.io.Serializable;
 
 public interface Resource<T> extends ResourceMetadata<T> {
 
-  /**
-   * Returns attribute value associated with the key during previous build.
-   */
   public <V extends Serializable> Serializable setAttribute(String key, V value);
 
-  public void addMessage(int line, int column, String message, MessageSeverity severity, Throwable cause);
+  public void addMessage(int line, int column, String message, MessageSeverity severity,
+      Throwable cause);
 
   public Output<File> associateOutput(Output<File> output);
 
