fd8a35449e3a16c131d5443a43295480081bd451
Igor Fedorenko <igor@ifedorenko.com>
2015-03-07 09:07:10 -0500

    YYY cleanup
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- incrementalbuild/src/main/java/io/takari/incrementalbuild/ResourceStatus.java
+++ incrementalbuild/src/main/java/io/takari/incrementalbuild/ResourceStatus.java
@@ -3,22 +3,22 @@ package io.takari.incrementalbuild;
 public enum ResourceStatus {
 
   /**
-   * Input is new in this build, i.e. it was not present in the previous build.
+   * Resource is new in this build, i.e. it was not present in the previous build.
    */
   NEW,
 
   /**
-   * Input itself changed or any of its included inputs changed or was removed since last build.
+   * Resource changed since previous build.
    */
   MODIFIED,
 
   /**
-   * Input itself and all includes inputs, if any, did not change since last build.
+   * Resource did not changed since previous build.
    */
   UNMODIFIED,
 
   /**
-   * Input was removed since last build.
+   * Resource was removed since previous build.
    */
   REMOVED;
 }
